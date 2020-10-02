c82bf4096362254450216922c56015f54f9006cd
joseph <josephpgreen@gmail.com>
2020-10-02 14:51:16 -0400

    added icons
    new icons

---
+++ NewPortfolio/Scripts/customScripts.js
@@ -0,0 +1,21 @@
+﻿//find the min of the numbers entered
+
+//function calcMin() {
+//    var firstNum = Number(document.getElementById("num1").value);
+//    var secondNum = Number(document.getElementById("num2").value);
+//    var thirdNum = Number(document.getElementById("num3").value);
+//    var forthNum = Number(document.getElementById("num4").value);
+//    var results;
+
+//    // Math.min(firstNum, secondNum, thirdNum, forthNum);
+//    document.getElementById("num1", "num2", "num3", "num4").innerHTML =
+//        Math.min(firstNum, secondNum, thirdNum, forthNum);
+
+//    document.getElementById("results").innerHTML = results;
+
+
+
+//}
+
+
+
